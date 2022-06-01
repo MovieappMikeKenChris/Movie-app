659fa3635ebef4690a6e0e7cf00ac9f90df8219a
Michael Dickens <dickens.michael64@gmail.com>
2022-06-01 15:38:35 -0500

    feat: wokring better...

--- Movie-app.js
+++ Movie-app.js
@@ -2,27 +2,37 @@ import {MOVIE_APP_API} from "/keys.js"
 import {POSTER_API_KEY} from "/keys.js"
 import {POSTER_API} from "/keys.js"
 
+import {mapUserToRecord } from "/maps.js";
+import {
+    handleDisplayUpdate, handleDeleteView,
+    toggleModal, modal, handleDisplayProfile,
+    handleCreateUserView
+} from "/handlers.js";
+
 const loader = document.querySelector('#loader')
 
 const createMovie = ({title, rating}) => {
     document.getElementById("content").innerHTML +=
-     `
+        `
     <div class="poster">
         <h4>${title}</h4>
-        <h5>${rating}</h5>
+        <h5>Rating: ${rating}</h5>
     
     </div>
-   
     `
 }
-
 fetch(MOVIE_APP_API)
     .then($("#loader").addClass("hidden"))
     .then((res) => res.json())
     .then((res) => {
         console.log(res)
         res.forEach((movie) => {
-            createMovie(movie)
+            if (typeof movie.title === 'string' && movie.title !== '') {
+                createMovie(movie)
+            }
+
         })
     })
 
+
+$("#create").click(handleCreateUserView);
---
+++ maps.js
@@ -0,0 +1,98 @@
+/**
+ * It takes an object with the properties' id, firstName, lastName, picture, and title,
+ * and returns a string that represents a table row with those properties
+ * @returns A string of HTML that represents a table row.
+ */
+
+// We used object destructing in the prams instead of sending over the
+    // whole item we are only capturing the field we are going to use here.
+export const mapUserToRecord = ({id, firstName, lastName, picture, title}) => {
+        return `<tr data-id="${id}" >
+                       <td>
+                            <img src="${picture}">
+                       </td>
+                       <td data-id="${id}" class="user-record">${title}. ${firstName} ${lastName}</td>
+              
+                       <td>
+                            <button class="delete" value="${id}">X</button>
+                            <button class="edit" value="${id}">Edit</button>
+                       </td>
+                   </tr>`
+    };
+
+export const mapUserToUpdate = (data) => {
+    return createForm("update", data)
+
+}
+
+export const mapUserCreateForm = (name = "create") => {
+
+    const formDefaults = {
+        title: "",
+        rating: ""
+    }
+    return createForm(name, formDefaults);
+}
+
+export const createForm = (name, {title, rating}) => {
+    if (typeof title !== "string" && typeof rating !== "number") {
+        alert("Input is invalid")
+
+    }
+
+    return `
+        <form name="${name}">
+        <label for="field1">Title</label><input type="text" name="title" value="${title.toUpperCase()}" id="field1">
+        <label for="field2">Rating</label><input type="text" name="rating" value="${rating}" id="field2">
+       
+    </form>
+    `
+}
+
+
+export const mapUserToView =
+    ({id, title, firstName, lastName, gender, email, dateOfBirth, phone, picture, location}) => {
+
+        // handle Z in data for timezone, might need to add back for update
+        dateOfBirth = dateOfBirth.slice(0, dateOfBirth.length - 1);
+
+        console.log("location:", location)
+        return `
+       <div class="profile">
+                <section class="header">
+                    <img src="${picture}" alt="${firstName} ${lastName}" class="profile-img" />
+                    <h3 class="profile-header">${firstName} ${lastName}</h3>
+                    <p class="sub-header"><a href="mailto:">${email}</a></p>
+                </section>
+
+                <section class="location">
+                    <h4>Address</h4>
+                    <div class="address">${location.street}</div>
+                    <div class="address">${location.city}, ${location.state}</div>
+                </section>
+
+                <section class="details">
+                    <h4>Details</h4>
+                    <div>Phone: <span>${phone}</span> </div>
+                    <div>Gender: <span>${gender}</span></div>
+                    <div>Title:  <span>${title}</span></div>
+                </section>
+       </div>
+    `
+    }
+
+export const mapButtonsForUpdate = (id, type = 'update') => {
+    return ` <form>
+          <button class="confirm ${type}" value="${id}">Confirm</button>
+          <button class="cancel">Cancel</button>
+            </form>`
+}
+
+export const mapUserToDelete = (id) => {
+    return `<form>
+          <button class="confirm delete" value="${id}">Confirm</button>
+          <button class="cancel">Cancel</button>
+    </form>
+  
+    `
+}
