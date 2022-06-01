87316ff8b01359196ed3f36fe659333ef1f07db6
Chris Merritt <christopher.aa.merritt@gmail.com>
2022-06-01 12:10:49 -0500

    add resource links for iamges

--- Movie-app.js
+++ Movie-app.js
@@ -1,6 +1,12 @@
 import {MOVIE_APP_API} from "/keys.js"
+import {POSTER_API_KEY} from "/keys.js"
+import {POSTER_API} from "/keys.js"
 
 const loader = document.querySelector('#loader')
+
+
+const createMovie = ({})
+
 
 fetch(MOVIE_APP_API)
     .then($("#loader").addClass("hidden"))
@@ -8,4 +14,8 @@ fetch(MOVIE_APP_API)
     .then((res) => {
         console.log(res)
     })
+    //     res.data.forEach((movie) => {
+    //         createMovie(movie)
+    //     })
+    // })
 
--- keys.js
+++ keys.js
@@ -1,2 +1,6 @@
 
 export const MOVIE_APP_API = "https://heavenly-assorted-log.glitch.me/movies"
+
+export const POSTER_API_KEY = "46a3befd"
+
+export const POSTER_API = "http://img.omdbapi.com/?apikey=46a3befd"
