3337e80d73705d45d887c232fe7ba0311b2e3209
Chris Merritt <christopher.aa.merritt@gmail.com>
2022-06-06 14:20:24 -0500

    change api old glitch timed out

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
 
-export const MOVIE_APP_API = "https://heavenly-assorted-log.glitch.me/movies/"
+export const MOVIE_APP_API = "https://first-quill-cuticle.glitch.me/movies/"
 
 export const POSTER_API = "https://www.omdbapi.com/?apikey=d7f835c7"
